d0f168ce78215ad07e31e0483ae9b4584ee4f367
usfbelhadj <usf.belhadj@gmail.com>
2021-10-26 18:42:11 +0100

    Aplha

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        Console.WriteLine("{0} {1}", number % 10, number);
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(char aplha='a'; aplha<='z'; aplha++)
+                {
+                    Console.Write(aplha);
+                }
+        }
+    }
+}
